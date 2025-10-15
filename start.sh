2038c377075795bea06964139cffc515b72bdf27
Manuk-Han <aksdnr507@gmail.com>
2025-10-16 06:52:39 +0900

    change port

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-./gradlew :module:domain:app:bootRun --args='--server.port=7070 --spring.profiles.active=db'
-./gradlew :module:platform:auth:bootRun --args='--server.port=7071 --spring.profiles.active=db'
-./gradlew :module:registry:schema-registry:bootRun --args='--server.port=7072 --spring.profiles.active=db'
+./gradlew :module:domain:app:bootRun --args='--server.port=6060 --spring.profiles.active=db'
+./gradlew :module:platform:auth:bootRun --args='--server.port=6061 --spring.profiles.active=db'
+./gradlew :module:registry:schema-registry:bootRun --args='--server.port=6062 --spring.profiles.active=db'
 
 cd ~/data_mesh/docker
 # 1) 레지스트리(Postgres) 마이그레이션
