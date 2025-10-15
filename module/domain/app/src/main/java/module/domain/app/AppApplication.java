8bece941932517ed2db7bc02e223e3ec1a999317
Manuk-Han <aksdnr507@gmail.com>
2025-10-16 06:09:57 +0900

    fix feginClient error

--- module/domain/app/src/main/java/module/domain/app/AppApplication.java
+++ module/domain/app/src/main/java/module/domain/app/AppApplication.java
@@ -2,7 +2,9 @@ package module.domain.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "module.domain.app.feign")
 @SpringBootApplication(scanBasePackages = "module")
 public class AppApplication {
 
